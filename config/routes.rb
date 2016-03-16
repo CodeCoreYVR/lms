1ced07e0e125cc057557ff2b2368bcb7d55abfae
kaiaolson <kaiaolson@gmail.com>
2016-03-16 08:27:34 -0700

    restrict password resets controller to necessary actions, delete duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  get 'password_resets/new'
 
   resources :users do
     get "/change_password" => "users#change_password"
   end
-  resources :password_resets
+  resources :password_resets, only: [:new, :create, :edit, :update]
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
   end
